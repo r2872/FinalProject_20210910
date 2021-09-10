bee2b9c3c0272bd2bacf623170849df5793ebf60
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-10 12:41:24 +0900

    로그인 성공시 -> 내 정보 추가요청 - JSONObject 형태로 돌아옴. 로그확인

--- app/src/main/java/com/r2872/finalproject_20210910/LoginActivity.kt
+++ app/src/main/java/com/r2872/finalproject_20210910/LoginActivity.kt
@@ -4,14 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
+import com.facebook.*
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.r2872.finalproject_20210910.databinding.ActivityLoginBinding
-import java.util.*
+import org.json.JSONObject
 
 
 class LoginActivity : BaseActivity() {
@@ -36,30 +33,65 @@ class LoginActivity : BaseActivity() {
         binding.facebookLoginBtn.setOnClickListener {
 
 //            우리가 붙인 버튼 기능 활용
+
+//            커스텀 버튼에, 로그인 하고 돌아온 Callback 을 따로 설정.
+            LoginManager.getInstance()
+                .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(result: LoginResult?) {
+
+                        Log.d("로그인 성공", "커스텀버튼으로 성공")
+//                    페이스북에 접근할 수 있는 토큰이 생겨 있다. => 활용.
+//                    나의 (로그인한사람) 정보 (GraphRequest) 를 받아오는데 활용.
+                        val graphRequest = GraphRequest.newMeRequest(
+                            result?.accessToken,
+                            object : GraphRequest.GraphJSONObjectCallback {
+                                override fun onCompleted(
+                                    jsonObj: JSONObject?,
+                                    response: GraphResponse?
+                                ) {
+
+                                    Log.d("내 정보 내용", jsonObj.toString())
+                                }
+                            })
+//                        위에서 정리한 내용을 들고, 내 정보를 실제로 요청.
+                        graphRequest.executeAsync()
+                    }
+
+                    override fun onCancel() {
+
+
+                    }
+
+                    override fun onError(error: FacebookException?) {
+
+
+                    }
+                })
+
             LoginManager.getInstance()
                 .logInWithReadPermissions(this, listOf("public_profile"))
         }
 
         // Callback registration
-        binding.loginButton.registerCallback(
-            callbackManager,
-            object : FacebookCallback<LoginResult?> {
-                override fun onSuccess(loginResult: LoginResult?) {
-                    // App code
-
-                    Log.d("확인용", loginResult.toString())
-                    val accessToken = AccessToken.getCurrentAccessToken()
-                    Log.d("페북토큰", accessToken.token.toString())
-                }
-
-                override fun onCancel() {
-                    // App code
-                }
-
-                override fun onError(exception: FacebookException) {
-                    // App code
-                }
-            })
+//        binding.loginButton.registerCallback(
+//            callbackManager,
+//            object : FacebookCallback<LoginResult?> {
+//                override fun onSuccess(loginResult: LoginResult?) {
+//                    // App code
+//
+//                    Log.d("확인용", loginResult.toString())
+//                    val accessToken = AccessToken.getCurrentAccessToken()
+//                    Log.d("페북토큰", accessToken.token.toString())
+//                }
+//
+//                override fun onCancel() {
+//                    // App code
+//                }
+//
+//                override fun onError(exception: FacebookException) {
+//                    // App code
+//                }
+//            })
     }
 
     override fun setValues() {
