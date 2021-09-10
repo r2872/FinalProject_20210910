44f901310c3100326350e3722231e2da9a6f673b
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-10 16:04:39 +0900

    API 엔드포인트 명시 인터페이스 추가  - 회원가입 API 사용 예시

---
+++ app/src/main/java/com/r2872/finalproject_20210910/web/ServerAPIService.kt
@@ -0,0 +1,19 @@
+package com.r2872.finalproject_20210910.web
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.PUT
+
+interface ServerAPIService {
+
+    @FormUrlEncoded
+    @PUT("/user")
+    fun putRequestSignUp(
+        @Field("email") email: String,
+        @Field("password") pw: String,
+        @Field("nick_name") nickname: String
+    ) {
+
+
+    }
+}
