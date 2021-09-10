294dc8229859d25845d7222bddd31e76cc1d0c2d
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-10 16:45:21 +0900

    회원가입 성공 / 실패 나눠서 처리  - 실패시에는 JSONObject 로 변환 -> 재 파싱  - runOnUiThread 고민 필요 없다.

--- app/src/main/java/com/r2872/finalproject_20210910/SignUpActivity.kt
+++ app/src/main/java/com/r2872/finalproject_20210910/SignUpActivity.kt
@@ -2,9 +2,11 @@ package com.r2872.finalproject_20210910
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.r2872.finalproject_20210910.databinding.ActivitySignUpBinding
 import com.r2872.finalproject_20210910.datas.BasicResponse
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -36,7 +38,28 @@ class SignUpActivity : BaseActivity() {
                         response: Response<BasicResponse>
                     ) {
 
-                        Log.d("retrofit", response.body().toString())
+//                        response.body() -> 응답 코드 200번이어야 들어있다. (성공)
+//                        가입실패 / 로그인 실패 -> 응답 코드 400 -> errorBody 에서 따로 찾아야함. (실패)
+
+                        if (response.isSuccessful) {
+                            val basicResponse = response.body()!!
+                            Log.d("서버 메세지", basicResponse.message)
+                            Toast.makeText(mContext, basicResponse.message, Toast.LENGTH_SHORT)
+                                .show()
+                            finish()
+                        } else {
+//                            어떤 이유던 성공이 아닌 상황.
+                            val errorBodyStr = response.errorBody()!!.string()
+
+//                            단순 JSON 형태의 String 으로 내려옴. => JSONObject 형태로 가공 필요.
+                            Log.d("에러 경우", errorBodyStr)
+                            val jsonObj = JSONObject(errorBodyStr)
+                            val message = jsonObj.getString("message")
+
+//                            runOnUiThread 를 해주지 않아도 UI 접근 가능.
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+
                     }
 
                     override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
