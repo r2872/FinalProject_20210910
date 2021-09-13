cac795b85426351bfaa03528179408863edf9c4d
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-13 12:05:23 +0900

    시간 선택 기능 진행

--- app/src/main/java/com/r2872/finalproject_20210910/EditAppoinmentActivity.kt
+++ app/src/main/java/com/r2872/finalproject_20210910/EditAppoinmentActivity.kt
@@ -5,11 +5,14 @@ import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.r2872.finalproject_20210910.databinding.ActivityEditAppoinmentBinding
+import java.text.SimpleDateFormat
+import java.time.Year
 import java.util.*
 
 class EditAppoinmentActivity : BaseActivity() {
 
     private lateinit var binding: ActivityEditAppoinmentBinding
+    private val mSelectedDateTime = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +24,29 @@ class EditAppoinmentActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        binding.datePickBtn.setOnClickListener {
+//        확인 버튼이 눌리면?
+        binding.addBtn.setOnClickListener {
+
+//            입력한 값들 받아오기
+//            1. 일정 제목
+            val inputTitle = binding.titleEdt.text.toString()
+
+//            2. 약속 일시? -> "2021-09-13 11:11" String 변환까지.
+            val inputDate = binding.selectedDateTxt.text.toString()
+
+//            3. 약속 장소?
+//            - 장소 이름
+            val inputPlaceName = binding.placeSearchEdt.text.toString()
+
+//            - 장소 위도 / 경도 ?
+
+        }
+
+        binding.dateTxt.setOnClickListener {
 
             showDatePicker()
         }
-        binding.timePickBtn.setOnClickListener {
+        binding.timeTxt.setOnClickListener {
 
             showTimePicker()
         }
@@ -36,17 +57,40 @@ class EditAppoinmentActivity : BaseActivity() {
     }
 
     private fun showDatePicker() {
-        val cal = Calendar.getInstance()
-        DatePickerDialog(mContext, DatePickerDialog.OnDateSetListener { datePicker, y, m, d ->
-            binding.dateTxt.text = "$y - ${m+1} - $d"
-        }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+        DatePickerDialog(
+            mContext,
+            DatePickerDialog.OnDateSetListener { _, y, m, d ->
+                mSelectedDateTime.apply {
+                    set(Calendar.YEAR, y)
+                    set(Calendar.MONTH, m)
+                    set(Calendar.DAY_OF_MONTH, d)
+                }
+                val sdf = SimpleDateFormat("yyyy-MM-dd (E)")
+
+                binding.selectedDateTxt.text = sdf.format(mSelectedDateTime.time).toString()
+            },
+            mSelectedDateTime.get(Calendar.YEAR),
+            mSelectedDateTime.get(Calendar.MONTH),
+            mSelectedDateTime.get(Calendar.DAY_OF_MONTH)
+        ).show()
     }
 
     private fun showTimePicker() {
-        val cal = Calendar.getInstance()
-        TimePickerDialog(mContext, TimePickerDialog.OnTimeSetListener { timePicker, h, m ->
-            binding.timeTxt.text = "$h - $m"
-        }, cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), true).show()
+        TimePickerDialog(
+            mContext,
+            TimePickerDialog.OnTimeSetListener { _, h, m ->
+                mSelectedDateTime.apply {
+                    set(Calendar.HOUR_OF_DAY, h)
+                    set(Calendar.MINUTE, m)
+                }
+                val sdf = SimpleDateFormat("yyyy-MM-dd (E) a h:mm")
+
+                binding.selectedDateTxt.text = sdf.format(mSelectedDateTime.time).toString()
+            },
+            mSelectedDateTime.get(Calendar.HOUR_OF_DAY),
+            mSelectedDateTime.get(Calendar.MINUTE),
+            false
+        ).show()
     }
 
 
