06709dc24a2553051f0d1df7957ba11b12f7968c
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-16 12:43:28 +0900

    BaseFragment 에서 retrofit 관련 변수도 물려 주도록.  => 프래그먼트도, 액티비티처럼 API 호출 가능.

--- app/src/main/java/com/r2872/finalproject_20210910/fragments/BaseFragment.kt
+++ app/src/main/java/com/r2872/finalproject_20210910/fragments/BaseFragment.kt
@@ -3,14 +3,22 @@ package com.r2872.finalproject_20210910.fragments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.r2872.finalproject_20210910.web.ServerAPI
+import com.r2872.finalproject_20210910.web.ServerAPIService
+import retrofit2.Retrofit
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var mContext: Context
+    private lateinit var retrofit: Retrofit
+    lateinit var apiService: ServerAPIService
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         mContext = requireContext()
+        retrofit = ServerAPI.getRetrofit(mContext)
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
 
     abstract fun setupEvents()
