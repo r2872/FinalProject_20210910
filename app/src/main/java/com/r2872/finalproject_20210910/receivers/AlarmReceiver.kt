7366d538839eb7b493c243d99f72ec9a2b98d961
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-23 12:37:48 +0900

    실제 알림 울리기 까지

--- app/src/main/java/com/r2872/finalproject_20210910/receivers/AlarmReceiver.kt
+++ app/src/main/java/com/r2872/finalproject_20210910/receivers/AlarmReceiver.kt
@@ -2,12 +2,16 @@ package com.r2872.finalproject_20210910.receivers
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.r2872.finalproject_20210910.MainActivity
+import com.r2872.finalproject_20210910.R
 
 class AlarmReceiver : BroadcastReceiver() {
 
@@ -22,6 +26,36 @@ class AlarmReceiver : BroadcastReceiver() {
 
 //        알림 - Oreo 버전 이후로는 채널을 설정해야함. 그 이전 버전에선 하면 안됨.
         createNotificationChannel()
+
+//        채널설정이 끝났다. => 실제 알림 울릴 준비 OK. 실제로 울리자.
+        deliverNotification(context)
+    }
+
+    private fun deliverNotification(context: Context) {
+
+//        알림을 누르면 어느 화면으로 갈건지?
+        val contentIntent = Intent(context, MainActivity::class.java)
+
+//        실제로 알림이 눌릴때 까지 대기하는 Intent.
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            ALARM_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+//        알림에 대한 정보 설정.
+        val notiBuilder = NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher) // mipmap 이 아니면 일부 폰에서 앱이 죽음. 모양을 바꾸려면 투명 배경 활용.
+            .setContentTitle("테스트 알람")
+            .setContentTitle("알람이 울립니다.") // 메세지 개념으로 생각.
+            .setContentIntent(pendingIntent) // 대기 Intent 사용. => 그 안에 contentIntent 도 같이 첨부.
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL) // 기본세팅 -> 알림소리, 진동패턴 등 커스터마이징 가능
+
+//        만들어진 정보를 종합해서 실제로 알림 발생
+        mNotificationManager.notify(ALARM_ID, notiBuilder.build())
     }
 
     private fun createNotificationChannel() {
